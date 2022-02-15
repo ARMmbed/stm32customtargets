6fde5d103d9f3f67036acc79ab7afdc40ba52a36
Jerome Coutant <jerome.coutant@st.com>
2022-02-15 15:56:18 +0100

    ci_build script: skip small profile MCU

--- aci_build.py
+++ aci_build.py
@@ -275,7 +275,7 @@ else:
     ## START BUILD
 
     def CheckBaremetalSupport(target_name):
-        baremetal_target_list = ["G030", "G030", "G041", "G050", "G051", "G061", "F030"]
+        baremetal_target_list = ["G030", "G031", "G041", "G050", "G051", "G061", "F030", "L053", "WB15"]
         for InList in baremetal_target_list:
             if InList in target_name:
                 return True
